40ae3540906734f9d984120e86dcf8d4577d119f
Matheus Soares <matheussilvasoares43@gmail.com>
2022-08-27 17:41:04 -0300

    oi

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -6,10 +6,26 @@
       <button class="button" @click="alterarTema">
         {{ textoBotao }}
       </button>
+      <nav class="panel mt-5">
+        <ul>
+          <li>
+            <router-link to="/" class="link">
+              <i class="fas fa-tasks"></i>
+              tarefas
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/projetos" class="link">
+              <i class="fas fa-project-diagram"></i>
+              Projetos
+            </router-link>
+          </li>
+        </ul>
+      </nav>
   </header>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -50,5 +66,21 @@ header {
     padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel li {
+  margin: 8px 0;
+}
+
+.link {
+  color: #fff;
+}
+
+.link:hover {
+  color: #FAF0CA;
+}
+
+.link.router-link-active {
+  color: #FAF0CA;
 }
 </style>
