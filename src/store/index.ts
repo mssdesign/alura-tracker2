aa7045b8af15b5b1eb0cd4ac119a9d1d2dc64384
Matheus Soares <matheussilvasoares43@gmail.com>
2022-10-08 20:49:11 -0300

    Aula 4 Módulo 4

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,37 +1,63 @@
-import IProjeto from "@/interfaces/IProjeto";
-import { InjectionKey } from "vue";
-import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipo-mutacoes";
+import { INotificacao, TipoNotificacao } from '@/interfaces/INotificacao'
+import IProjeto from '@/interfaces/IProjeto'
+import { InjectionKey } from 'vue'
+import { createStore, Store, useStore as vuexUseStore } from 'vuex'
+import {
+  ADICIONA_PROJETO,
+  ALTERA_PROJETO,
+  EXCLUIR_PROJETO,
+} from './tipo-mutacoes'
 
 interface Estado {
-    projetos: IProjeto[]
+  projetos: IProjeto[]
+  notificacoes: INotificacao[]
 }
 
-export const key: InjectionKey<Store<Estado>> = Symbol()    //Configurando chave de acesso ao store
+export const key: InjectionKey<Store<Estado>> = Symbol() //Configurando chave de acesso ao store
 
 export const store = createStore<Estado>({
-    state: {
-        projetos: []
+  state: {
+    projetos: [],
+    notificacoes: [
+      {
+        id: 1,
+        texto: 'Uma notificação de sucesso',
+        titulo: 'sucesso',
+        tipo: TipoNotificacao.SUCESSO
+      },
+      {
+        id: 2,
+        texto: 'Uma notificação de atenção',
+        titulo: 'atenção',
+        tipo: TipoNotificacao.ATENCAO
+      },
+      {
+        id: 3,
+        texto: 'Uma notificação de falha',
+        titulo: 'falha',
+        tipo: TipoNotificacao.FALHA
+      }
+    ],
+  },
+  mutations: {
+    [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
+      const projeto = {
+        id: new Date().toISOString(),
+        nome: nomeDoProjeto,
+      } as IProjeto
+      state.projetos.push(projeto)
     },
-    mutations: {
-        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
-            const projeto= {
-                id: new Date().toISOString(),
-                nome: nomeDoProjeto
-            } as IProjeto
-            state.projetos.push(projeto)
-        },
-        [ALTERA_PROJETO](state, projeto: IProjeto) {
-            const index = state.projetos.findIndex(proj => proj.id == projeto.id)
-            state.projetos[index] = projeto
-        },
-        [EXCLUIR_PROJETO](state, id: string) {
-            state.projetos = state.projetos.filter(proj => proj.id != id)
-        }
-    }
+    [ALTERA_PROJETO](state, projeto: IProjeto) {
+      const index = state.projetos.findIndex((proj) => proj.id == projeto.id)
+      state.projetos[index] = projeto
+    },
+    [EXCLUIR_PROJETO](state, id: string) {
+      state.projetos = state.projetos.filter((proj) => proj.id != id)
+    },
+  },
 })
 
 //Exportando um useStore personalizado
 export function useStore(): Store<Estado> {
-    return vuexUseStore(key)
+  return vuexUseStore(key)
 }
