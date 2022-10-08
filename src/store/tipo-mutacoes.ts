7037aa7334b855be6ce1dd8223815ce309978428
Matheus Soares <matheussilvasoares43@gmail.com>
2022-10-08 19:44:58 -0300

    Aula 5 Modulo 3

---
+++ src/store/tipo-mutacoes.ts
@@ -0,0 +1,4 @@
+//Criando variáveis globais e ajudando o typescript evitando erros de digitação semelhante a um model de objeto
+export const ADICIONA_PROJETO = 'ADICIONA_PROJETO'
+export const ALTERA_PROJETO = 'ALTERA_PROJETO' 
+export const EXCLUIR_PROJETO = 'EXCLUIR_PROJETO'
--- src/views/Projetos/Lista.vue
+++ src/views/Projetos/Lista.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <router-link to="/projetos/novo" class="button">
+        <router-link to="/novo" class="button">
           <span class="icon is-small">
             <i class="fas fa-plus"></i>
           </span>
@@ -21,11 +21,16 @@
                   <td>{{ projeto.id }}</td>
                   <td>{{projeto.nome}}</td>
                   <td>
-                    <router-link :to="`/projetos/${projeto.id}`" class="button">
+                    <router-link :to="`/${projeto.id}`" class="button">
                       <span class="icon is-small">
                         <i class="fas fa-pencil-alt"></i>
                       </span>
                     </router-link>
+                    <button class="button ml-2 is-danger" @click="excluir(projeto.id)">
+                      <span class="icon is-small">
+                      <i class="fas fa-trash"></i>
+                      </span>
+                    </button>
                   </td>
               </tr>
           </tbody>
@@ -36,13 +41,20 @@
   <script lang="ts">
   import { computed, defineComponent } from 'vue';
   import { useStore } from '@/store';
+import { EXCLUIR_PROJETO } from '@/store/tipo-mutacoes';
   
   export default defineComponent({
     name: 'Lista',
+    methods: {
+      excluir (id: string) {
+        this.store.commit(EXCLUIR_PROJETO, id)
+      }
+    },
     setup () {
       const store = useStore()
       return {
-        projetos: computed(() => store.state.projetos)
+        projetos: computed(() => store.state.projetos),
+        store
       }
     }
   })
