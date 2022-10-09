e29edaaaa26086620fa75643124dfb91c902fc4e
Matheus Soares <matheussilvasoares43@gmail.com>
2022-10-08 23:54:39 -0300

    Aula 2 Modulo 5

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -24,8 +24,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from '@/store';
-import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes';
+import { ALTERA_PROJETO, ADICIONA_PROJETO } from '@/store/tipo-mutacoes';
 import { TipoNotificacao } from '@/interfaces/INotificacao';
+import { notificacaoMixin } from '@/mixins/notificar';
 
 export default defineComponent({
   name: 'Formulario',
@@ -34,6 +35,7 @@ export default defineComponent({
       type: String
     }
   },
+  mixins: [notificacaoMixin],
   mounted() {
     if (this.id) {
       const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
@@ -56,13 +58,9 @@ export default defineComponent({
         this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)        
       }
       this.nomeDoProjeto = '';
-      this.store.commit(NOTIFICAR, {
-        titulo: 'Novo projeto foi salvo',
-        texto: 'Pronto',
-        tipo: TipoNotificacao.SUCESSO
-      })
+      this.notificar(TipoNotificacao.SUCESSO, 'Excelente!', 'O projeto foi cadastrado com sucesso.')
       this.$router.push('/projetos')
-    },
+    }
   },
   setup () {
     const store = useStore()
